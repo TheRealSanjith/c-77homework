6cf160717ffd0dd8b360aa0650436cd29e5e4b4e
TheRealSanjith <103978094+TheRealSanjith@users.noreply.github.com>
2022-10-18 20:36:29 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var first_name="Yate ";
+
+var last_name="logan";
+console.log(first_name);
+console.log(last_name);
+
+var a=9;
+var b=30;
+
+var x=a+b;
+console.log(x);
+
+var c=10;
+var d=3;
+
+var y=c-d;
+console.log(y);
+
+var z=first_name.concat(last_name);
+console.log(z);
